d79bdf62ec2b23975bdcaaa57932b6fdcdb58437
Swappnet <advance11ua@gmail.com>
2023-04-04 20:55:42 +0300

    cleaned

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -1,5 +1,3 @@
-import "./main.css";
-
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
---
+++ client/src/styles/GlobalStyle.ts
@@ -0,0 +1,37 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+ *,
+  *:before,
+  *:after {
+    box-sizing: border-box;
+    -webkit-box-sizing: border-box; /* Safari/Chrome, other WebKit */
+    -moz-box-sizing: border-box;    /* Firefox, other Gecko */
+    box-sizing: border-box;         /* Opera/IE 8+ */
+    margin: 0;
+    padding: 0;
+  }
+
+  
+  body,
+  html {
+    background-color: #ffffff;
+    margin: 0;
+    padding: 0;
+    font-size: 16px;
+    height: 100%;
+    width: 100%;
+    box-sizing: border-box;
+    -webkit-box-sizing: border-box; /* Safari/Chrome, other WebKit */
+    -moz-box-sizing: border-box;    /* Firefox, other Gecko */
+    box-sizing: border-box; 
+    *::-moz-selection { /* Code for Firefox */
+      background: #9e9e9e50;
+    }
+    *::selection {
+      background: #9e9e9e50;
+    }
+  }
+`;
+
+export default GlobalStyle;
