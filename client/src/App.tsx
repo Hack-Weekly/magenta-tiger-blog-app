88d9cd09b779c54fbecf7a9438b59b29ff37d09a
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2023-04-04 20:28:54 +0100

    Ready for pull

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,5 @@
-import Logo from "@/assets/logo.png";
-import HelloWorld from "@/components/HelloWorld/HelloWorld";
-
-import styles from "./App.module.css";
+import Nav from "./components/nav/Nav";
 
 export default function App() {
-  return (
-    <main className={styles.main}>
-      <img className={styles.logo} alt="React logo" width="400px" src={Logo} />
-      <HelloWorld msg="Hello React + TypeScript + Vite" />
-    </main>
-  );
+  return <Nav />;
 }
---
+++ client/src/components/nav/Nav.tsx
@@ -0,0 +1,75 @@
+import styled from "styled-components";
+import { ProfileButtonProps } from "@/types/navProps";
+
+const StyledNav = styled.nav`
+  width: 100%;
+  padding: 1rem;
+  background-color: white;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const StyledProfileBtn = styled.button<ProfileButtonProps>`
+  width: 3.5rem;
+  height: 3.5rem;
+  border-radius: 50%;
+  background-image: ${props => `url(${props.imageUrl})`};
+  background-size: contain;
+  background-position: center;
+`;
+
+const StyledMenuBtn = styled.button`
+  width: 2.5rem;
+  height: 2.5rem;
+  border-radius: 50%;
+  background-color: black;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.3rem;
+`;
+
+const StyledLine1Div = styled.div`
+  width: 1.5rem;
+  height: 0.2rem;
+  background-color: white;
+`;
+const StyledLine2Div = styled.div`
+  width: 1.5rem;
+  height: 0.2rem;
+  background-color: white;
+  margin-left: 0.5rem;
+`;
+const StyledLine3Div = styled.div`
+  width: 1.5rem;
+  height: 0.2rem;
+  background-color: white;
+`;
+const Nav = () => {
+  const user: boolean = true;
+
+  return (
+    <>
+      <StyledNav>
+        <>
+          <StyledMenuBtn>
+            <StyledLine1Div />
+            <StyledLine2Div />
+            <StyledLine3Div />
+          </StyledMenuBtn>
+          {/* swap with styled button when available
+          and update logic for user display */}
+          {user ? (
+            <StyledProfileBtn imageUrl="https://avatars.githubusercontent.com/u/101993818?v=4" />
+          ) : (
+            <button>Create account</button>
+          )}
+        </>
+      </StyledNav>
+    </>
+  );
+};
+
+export default Nav;
