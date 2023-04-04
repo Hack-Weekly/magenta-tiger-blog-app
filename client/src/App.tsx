ee625c1b48421d691a8f7377d4445c097a21adb9
Swappnet <advance11ua@gmail.com>
2023-04-04 23:58:47 +0300

    cleaned

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,4 @@
-import { Button } from "./components/Button";
 import GlobalStyle from "./styles/GlobalStyle";
-import { faHandPointLeft } from "@fortawesome/free-regular-svg-icons";
 
 export default function App() {
   return (
