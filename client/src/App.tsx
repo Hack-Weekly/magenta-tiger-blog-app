a55e827be1104dc71b60332c63018bcd068955a0
Swappnet <advance11ua@gmail.com>
2023-04-04 21:00:09 +0300

    cleaned

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,31 +1,9 @@
-import styled, {css} from 'styled-components';
-import styles from './App.module.css';
-import GlobalStyle from './components/global-styles/GlobalStyle';
-import { Button } from '@/components';
-
-// import { faBookmark } from '@fortawesome/free-regular-svg-icons';
-// import { faGithub } from '@fortawesome/free-brands-svg-icons'
-
-// const CustomBtn = styled(Button)`
-//   text-transform: uppercase;
-// `
+import GlobalStyle from "./styles/GlobalStyle";
 
 export default function App() {
   return (
-    <main className={styles.main}>
-      {/* CODE FOR TESTING BUTTON I CAN'T SET UP STORYBOOK :')
-      <div>
-        <GlobalStyle />
-        <Button variant='primary' shadow>Primary</Button>
-        <Button variant='secondary' shadow>Secondary</Button>
-        <Button variant='secondary' shadow transparent icon={faGithub} bold>Sign up with GitHub</Button>
-        <Button variant='secondary' icon={faBookmark} />
-        <Button variant='icon' transparent icon={faBookmark} />
-        <Button variant='text'>Text</Button>
-        <CustomBtn variant='text' active bold>Active Text</CustomBtn>
-        <Button danger>Danger</Button>
-        <Button disabled>Disabled</Button>
-      </div> */}
-    </main>
+    <>
+      <GlobalStyle />
+    </>
   );
 }
--- client/src/components/index.tsx
+++
@@ -1,3 +0,0 @@
-export * from './Button';
-export * from './global-styles'
-export * from './global-styles/GlobalStyle'
