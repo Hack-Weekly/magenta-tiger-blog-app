793ff4bbbde46d6e7e4f688de9b5f5c633c8b178
Joshua Ibbotson <joshua_ibbotson@hotmail.com>
2023-04-08 20:59:36 +0100

    BUILD - basic structure completed with use of styled components throughout.

--- client/src/components/enter/EnterLayout.tsx
+++ client/src/components/enter/EnterLayout.tsx
@@ -1,5 +1,6 @@
 import { Button } from "../Button";
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
 const StyledMainContainer = styled.div`
   display: flex;
@@ -10,7 +11,7 @@ const StyledMainContainer = styled.div`
   min-width: 100%;
 `;
 
-const StyledFormContainer = styled.div`
+const StyledCardWrapper = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: center;
@@ -18,15 +19,16 @@ const StyledFormContainer = styled.div`
 
   width: 100%;
   padding: 1rem;
-  box-shadow: 0px 0px 4px grey;
+  box-shadow: 0px 0px 1px grey;
   border-radius: 10px;
-  gap: 1rem;
+  gap: 2rem;
+  background-color: white;
 
-  @media (min-width: 680px) {
-    border: 1px solid grey;
+  @media (min-width: 500px) {
     margin-top: 1rem;
     margin-bottom: 1rem;
     max-width: 35rem;
+    padding: 3rem;
   }
 `;
 
@@ -35,6 +37,16 @@ const StyledTitleContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  h1 {
+    font-size: 1.6rem;
+    text-align: center;
+  }
+  h3 {
+    text-align: center;
+    font-weight: 400;
+    font-size: 1rem;
+  }
 `;
 
 const StyledBtnContainer = styled.div`
@@ -47,7 +59,7 @@ const StyledBtnContainer = styled.div`
 const StyledTextDiv = styled.div`
   text-align: center;
   display: flex;
-  flex-basis: 100%;
+
   align-items: center;
   color: rgba(0, 0, 0, 0.35);
 
@@ -60,17 +72,67 @@ const StyledTextDiv = styled.div`
     width: 6rem;
     font-size: 0px;
     line-height: 0px;
-    margin: 0px 16px;
+  }
+`;
+
+const StyledForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  gap: 1rem;
+
+  input[type="email"],
+  input[type="password"] {
+    height: 2.5rem;
+    padding-left: 0.3rem;
+    border-radius: 4px;
+    border: 1px solid grey;
+  }
+`;
+
+const StyledCheckBoxWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 0.5rem;
+
+  input[type="checkbox"] {
+    appearance: none;
+    background-color: #fff;
+    margin: 0;
+    font: inherit;
+    color: currentColor;
+    width: 1rem;
+    height: 1rem;
+    border: 0.05em solid grey;
+    border-radius: 0.3em;
+    transform: translateY(-0.075em);
+  }
+  input[type="checkbox"]:checked {
+    appearance: auto;
+  }
+`;
+
+const StyledLinkWrapper = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  padding: 1rem;
+
+  a,
+  a:link,
+  a:visited {
+    text-decoration: none;
+    color: blue;
   }
 `;
 
 export const EnterLayout = () => {
   return (
     <StyledMainContainer>
-      <StyledFormContainer>
+      <StyledCardWrapper>
         <StyledTitleContainer>
-          <h1>Welcome to Magenta tiger Blog</h1>
-          <h3>Built off hard work</h3>
+          <h1>Welcome to Magenta Tiger Blog</h1>
+          <h3>Built off passionate developers working mornings and nights</h3>
         </StyledTitleContainer>
         <StyledBtnContainer>
           <Button variant="primary" label="Continue with Github" />
@@ -78,13 +140,21 @@ export const EnterLayout = () => {
         <StyledTextDiv>
           Have a password? Continue with your email address
         </StyledTextDiv>
-        <form action="submit">
+        <StyledForm action="submit">
           <label htmlFor="email">Email</label>
           <input type="email" name="email" id="email" />
           <label htmlFor="password">Password </label>
           <input type="password" name="password" id="password" />
-        </form>
-      </StyledFormContainer>
+          <StyledCheckBoxWrapper>
+            <input type="checkbox" name="rememberMe" id="rememberMe" />{" "}
+            <label htmlFor="rememberMe">Remember me</label>
+          </StyledCheckBoxWrapper>
+          <Button variant="primary" label="Continue" />
+          <StyledLinkWrapper>
+            <Link to={"./resetpassword"}>I forgot my password</Link>
+          </StyledLinkWrapper>
+        </StyledForm>
+      </StyledCardWrapper>
     </StyledMainContainer>
   );
 };
