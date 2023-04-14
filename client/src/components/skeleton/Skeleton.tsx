ea05a642c46c5200367d4ef4846e461f345001d8
Swappnet <advance11ua@gmail.com>
2023-04-14 11:15:35 +0300

    updated skeleton

--- client/src/components/skeleton/Skeleton.tsx
+++ client/src/components/skeleton/Skeleton.tsx
@@ -1,62 +1,38 @@
-import styled, { keyframes } from "styled-components";
+import styled, { css } from "styled-components";
 import { Skeleton } from "@/types/src/styled-components/skeleton.types";
-import { BigSkeleton } from "./BigSkeleton";
 import { CompactSkeleton } from "./CompactSkeleton";
 import { PostDetailSkeleton } from "./PostDetailSkeleton";
 
-const shimmer = keyframes`
-  0% {
-    left: -100%;
-  }
-  100% {
-    left: 100%;
-  }
-`;
-
-const MainSkeletonWrapper = styled.div`
+const MainSkeletonWrapper = styled.div<Skeleton>`
+  padding: 2rem 0;
   width: 100%;
-  height: 100%;
   display: flex;
   flex-direction: column;
   align-items: flex-start;
   justify-content: center;
-  gap: 2rem;
+  gap: 1rem;
+  ${({ variant }) =>
+    variant === "home-all" &&
+    css`
+      @media (min-width: 680px) {
+        flex-wrap: wrap;
+        flex-direction: row;
+        justify-content: flex-start;
+      }
+    `}
   div {
     border: none;
-    border-radius: 4px;
+    border-radius: 0.2rem;
     position: relative;
     overflow: hidden;
-    &:after {
-      content: "";
-      position: absolute;
-      top: 0;
-      left: -100%;
-      width: 200%;
-      height: 100%;
-      opacity: 0.4;
-      background: linear-gradient(
-        to right,
-        transparent 0%,
-        #f8f8f8 50%,
-        transparent 100%
-      );
-      animation: ${shimmer} 1.5s infinite;
-    }
   }
 `;
 
-const SkeletonLoader = ({ variant = "home-recent" }: Skeleton) => {
+const SkeletonLoader = ({ variant = "home-all" }: Skeleton) => {
   const skeletons = Array.from({ length: 10 });
   return (
-    <MainSkeletonWrapper>
-      {variant === "home-recent" ? (
-        <>
-          <BigSkeleton></BigSkeleton>
-          {skeletons.map((_, count) => (
-            <CompactSkeleton key={count} />
-          ))}
-        </>
-      ) : variant === "home-all" ? (
+    <MainSkeletonWrapper variant={variant}>
+      {variant === "home-all" ? (
         <>
           {skeletons.map((_, count) => (
             <CompactSkeleton key={count} />
