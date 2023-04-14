52377af4bff44dd14aff24133ee85e93ca10c651
Gw3i <contact.johngoldco@gmail.com>
2023-04-14 04:43:38 +0200

    Get githubToke in homepage

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,8 +1,13 @@
 import StyledContainer from "@/components/StyledContainer";
 import { PostsSection } from "@/components/home/PostsSection";
 import { Sidebar } from "@/components/home/SidebarSection";
+import Cookies from "js-cookie";
 
 const Home = () => {
+  const userToken = Cookies.get("githubToken");
+
+  console.log(userToken);
+
   return (
     <StyledContainer variant="page">
       <Sidebar />
